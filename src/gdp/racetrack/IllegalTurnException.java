888fcb39b452913d0572ecb2d21b1b51498986f7
JojOatXGME <johannes.spangenberg@hotmail.de>
2013-01-26 21:30:31 +0100

    add a short class description to IllegalTurnException

--- src/gdp/racetrack/IllegalTurnException.java
+++ src/gdp/racetrack/IllegalTurnException.java
@@ -1,5 +1,8 @@
 package gdp.racetrack;
 
+/**
+ * This exception is thrown if a player does a illegal turn.
+ */
 public class IllegalTurnException extends RuntimeException {
 	private static final long serialVersionUID = -5141627377021338495L;
 
