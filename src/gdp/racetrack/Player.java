cff5d3f4240974841e1f77a6c2e02948aa090f6e
JojOatXGME <johannes.spangenberg@hotmail.de>
2013-01-29 14:04:22 +0100

    update and implement Player.java

--- src/gdp/racetrack/Player.java
+++ src/gdp/racetrack/Player.java
@@ -4,8 +4,24 @@ import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Player {
+	private Game game = null;
 
-	private final LinkedList<Vec2D> turnHistory = new LinkedList<Vec2D>();
+	// --- --- Initialization part --- ---
+
+	public Player() {
+		// do nothing
+	}
+
+	protected final void init(Game game) {
+		if (game != null)
+			throw new IllegalStateException("The player is already initialized");
+		
+		this.game = game;
+		
+		onLoad();
+	}
+
+	// --- --- Methods to override --- ---
 
 	/**
 	 * Elaborate the next turn of the player and return its destination.
@@ -13,12 +29,38 @@ public abstract class Player {
 	 */
 	public abstract Point turn();
 
+	protected void onLoad() {
+		// do nothing
+	}
+
+	protected void onUpdatePosition(Point oldPos, Point newPos) {
+		// do nothing
+	}
+
+	protected void onUpdateVelocity(Vec2D oldVelocity, Vec2D newVelocity) {
+		// do nothing
+	}
+
+	// --- --- Player logic --- ---
+
+	private final LinkedList<Vec2D> turnHistory = new LinkedList<Vec2D>();
+
+	private Point position = null;
+	private Vec2D velocity = null;
+
+	public final Game getGame() {
+		if (game == null)
+			throw new IllegalStateException("The player is not initialized yet");
+		
+		return game;
+	}
+
 	/**
 	 * Gets the actual position of the player as Vec2D from bottom left corner.
 	 * @return The position of the player
 	 */
 	public final Point getPosition() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+		return position;
 	}
 
 	/**
@@ -27,13 +69,12 @@ public abstract class Player {
 	 * @return The velocity of the last move
 	 */
 	public final Vec2D getVelocity() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+		return velocity;
 	}
 
 	/**
-	 * Gets a history of the movements of the player.
-	 * use points or velocities?
-	 * @return History of movements
+	 * Gets a history of positions of the player.
+	 * @return History of positions
 	 */
 	public final List<Vec2D> getTurnHistory() {
 		throw new UnsupportedOperationException("The method is not implemented yet.");
@@ -43,16 +84,20 @@ public abstract class Player {
 	 * Sets the position of this player.
 	 * @param position The new position of the player
 	 */
-	final void setPosition(Point position) {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+	public final void setPosition(Point position) {
+		Point oldPos = this.position;
+		this.position = position;
+		onUpdatePosition(oldPos, position);
 	}
 
 	/**
 	 * Sets the velocity of this player.
 	 * @param velocity The new velocity of the player
 	 */
-	final void setVelocity(Vec2D velocity) {
-		
+	public final void setVelocity(Vec2D velocity) {
+		Vec2D oldVelocity = this.velocity;
+		this.velocity = velocity;
+		onUpdateVelocity(oldVelocity, velocity);
 	}
 
 }
