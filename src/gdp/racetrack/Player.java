fd9d9df732a7cb9b2aea7a5b6f4e8b41529cb609
JojOatXGME <johannes.spangenberg@hotmail.de>
2013-01-29 13:30:20 +0100

    Revert "Revert "Implementation interface tweak (Sorry, Kasper!)""
    This reverts commit cce1e2473f75a50acc28475f082c2f52ab0a9248.

--- src/gdp/racetrack/Player.java
+++ src/gdp/racetrack/Player.java
@@ -1,58 +1,74 @@
-package gdp.racetrack;
+//Ich weiß nicht, wie ich eine neue Datei über das Web-Interface erstelle, also hier mein Vorschlag:
+
+package vectorAce;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.awt.Point;
 
 public abstract class Player {
 
-	private final LinkedList<Vec2D> turnHistory = new LinkedList<Vec2D>();
+	protected final LinkedList<Point> turnHistory = new LinkedList<Point>();
 
 	/**
 	 * Elaborate the next turn of the player and return its destination.
+	 * 
 	 * @return The destination point of the next turn
 	 */
-	public abstract Point turn();
+	public final Point turn() {
+		Point turn = getTurn();
+		turnHistory.add(turn);
+		return turn;
+	}
+
+	protected abstract Point getTurn();
 
 	/**
 	 * Gets the actual position of the player as Vec2D from bottom left corner.
+	 * 
 	 * @return The position of the player
 	 */
-	public final Point getPosition() {
+	public Point getPosition() {
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
 
 	/**
-	 * Gets the current "velocity" of the player.
-	 * It is normally the way which the player moved in the last round.
+	 * Gets the current "velocity" of the player. It is normally the way which
+	 * the player moved in the last round.
+	 * 
 	 * @return The velocity of the last move
 	 */
-	public final Vec2D getVelocity() {
+	public Point getVelocity() {
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
 
 	/**
-	 * Gets a history of the movements of the player.
-	 * use points or velocities?
+	 * Gets a history of the positions of the player.
+	 * 
 	 * @return History of movements
 	 */
-	public final List<Vec2D> getTurnHistory() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+	public final List<Point> getTurnHistory() {
+		return turnHistory;
 	}
 
 	/**
 	 * Sets the position of this player.
-	 * @param position The new position of the player
+	 * 
+	 * @param position
+	 *            The new position of the player
 	 */
-	final void setPosition(Point position) {
+	public void setPosition(Point position) {
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
 
 	/**
 	 * Sets the velocity of this player.
-	 * @param velocity The new velocity of the player
+	 * 
+	 * @param velocity
+	 *            The new velocity of the player
 	 */
-	final void setVelocity(Vec2D velocity) {
-		
+	public void setVelocity(Point velocity) {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
 
 }
