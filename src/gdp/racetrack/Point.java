db17ec4e5cee1434534b09463fe1cff6b7c3c1e2
JojOatXGME <johannes.spangenberg@hotmail.de>
2013-01-29 17:30:12 +0100

    add some JavaDoc comments to Point.java

--- src/gdp/racetrack/Point.java
+++ src/gdp/racetrack/Point.java
@@ -1,17 +1,34 @@
 package gdp.racetrack;
 
 public class Point {
-	@Deprecated public final int x;
-	@Deprecated public final int y;
+	/**
+	 * @deprecated It is better to use {@link #getX()}
+	 */
+	public final int x;
+	/**
+	 * @deprecated It is better to use {@link #getY()}
+	 */
+	public final int y;
 
 	private final Vec2D vec;
 	private final Map map;
 
-	public Point(int x, int y, Map map) {
+	/**
+	 * Creates a Point which represent the given position (x,y) on the given map.
+	 * @param x The x-coordinate of the point
+	 * @param y The y-coordinate of the point
+	 * @param map The Map on which is the point
+	 */
+	public Point(final int x, final int y, final Map map) {
 		this(new Vec2D(x,y), map);
 	}
 
-	public Point(Vec2D vec, Map map) {
+	/**
+	 * Creates a Point which represent the given position on the given map.
+	 * @param vec The Vector from (0,0) to this point
+	 * @param map The Map on which is the point
+	 */
+	public Point(final Vec2D vec, final Map map) {
 		this.vec = vec;
 		this.map = map;
 		
@@ -19,16 +36,38 @@ public class Point {
 		this.y = vec.y;
 	}
 
+	/**
+	 * Gets the x-coordinate of the point.
+	 * It represents the horizontal position. 0 is the left side.
+	 * @return The x-coordinate of the point
+	 */
 	public int getX() {
 		return vec.x;
 	}
 
+	/**
+	 * Gets the y-coordinate of the point.
+	 * It represents the vertical position. 0 is the bottom.
+	 * @return The y-coordinate of the point
+	 */
 	public int getY() {
 		return vec.y;
 	}
 
+	/**
+	 * Gets the vector which represent the line from (0,0) to this point.
+	 * @return The vector from (0,0) to this point
+	 */
 	public Vec2D getVec() {
 		return vec;
+	}
+
+	/**
+	 * Gets the Map in which the point is set.
+	 * @return The Map of this point
+	 */
+	public Map getMap() {
+		return map;
 	}
 
 	public Point add(Vec2D summand) {
@@ -44,17 +83,9 @@ public class Point {
 	}
 
 	/**
-	 * Gets the Map in which the point is set.
-	 * @return The Map of this point
-	 */
-	public Map getMap() {
-		return map;
-	}
-
-	/**
 	 * Check whether the point is a part of the track or not.
 	 * <br>
-	 * This method use Map.isTrack(Point).
+	 * This method use {@link Map#isTrack(Point)}.
 	 * 
 	 * @return true if the point is part of the track, false otherwise
 	 */
@@ -65,7 +96,7 @@ public class Point {
 	/**
 	 * Gets the Type of the point on the map.
 	 * <br>
-	 * This method use Map.getPointType(Point) to gets the type.
+	 * This method use {@link Map#getPointType(Point)} to gets the type.
 	 * 
 	 * @return The Type of the point
 	 */
