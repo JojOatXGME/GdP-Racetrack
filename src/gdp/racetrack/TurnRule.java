56314b2ce14ca20d344b311531776de105285ed0
JojOatXGME <johannes.spangenberg@hotmail.de>
2013-01-31 13:10:33 +0100

    update interface TurnRule

--- src/gdp/racetrack/TurnRule.java
+++ src/gdp/racetrack/TurnRule.java
@@ -11,6 +11,16 @@ public interface TurnRule {
 	public boolean isTurnAllowed(Player player, Point destination);
 
 	/**
+	 * Check whether the given Turn would be allowed for a player
+	 * on the given position with the given velocity.
+	 * @param start The players start position
+	 * @param velocity The players velocity
+	 * @param destination The destination of the turn
+	 * @return true if the turn would be allowed, false otherwise
+	 */
+	public boolean isTurnAllowed(Point start, Vec2D velocity, Point destination);
+
+	/**
 	 * Gets all allowed turns which the given player can do.
 	 * @param player The Player to be checked for
 	 * @return A array of points where the player can move to
