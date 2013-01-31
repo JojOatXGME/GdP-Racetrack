8110999657a214f4e9c0d1f9d940c84865fd99c2
JojOatXGME <johannes.spangenberg@hotmail.de>
2013-01-31 14:58:44 +0100

    update RuleSet.java

--- src/gdp/racetrack/RuleSet.java
+++ src/gdp/racetrack/RuleSet.java
@@ -22,11 +22,32 @@ public class RuleSet {
 	}
 
 	/**
+	 * Check whether the given turn would be allowed ot not.
+	 * @param position The players position
+	 * @param velocity The players velocity
+	 * @param destination The destination of the requested turn
+	 * @return true if the turn would be allowed or false otherwise
+	 */
+	public boolean isTurnAllowed(Point position, Vec2D velocity, Point destination) {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	/**
 	 * Gets all allowed turns which the given player can do.
 	 * @param player The Player to be checked for
-	 * @return A array of points where the player can move to
+	 * @return A array of possible turns
 	 */
-	public Vec2D[] getAllowedTurns(Player player) {
+	public Turn[] getAllowedTurns(Player player) {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	/**
+	 * Gets all allowed turns which a player with the given position and velocity can do.
+	 * @param position The Players position
+	 * @param velocity The players velocity
+	 * @return A array possible turns
+	 */
+	public Turn[] getAllowedTurns(Point position, Vec2D velocity) {
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
 
