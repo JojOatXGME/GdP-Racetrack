fab8ace5f932b12a6c47c9fdc016ec22f1da8c43
Eeep <dewa94@gmx.net>
2013-02-06 12:56:16 +0100

    update PlayerInfo

--- src/gdp/racetrack/PlayerInfo.java
+++ src/gdp/racetrack/PlayerInfo.java
@@ -6,11 +6,13 @@ public class PlayerInfo {
 
 	private Point newPos;
 	private Vec2D velocity;
+	private boolean pathValid;
 
-	public PlayerInfo(Point newPos, Point lastPos, Vec2D velocity) {
+	public PlayerInfo(Point newPos, Point lastPos, Vec2D velocity, boolean pathValid) {
 		this.newPos = newPos;
 		this.lastPos = lastPos;
 		this.velocity = velocity;
+		this.pathValid = pathValid;
 	}
 
 	public Point getNewPos() {
@@ -25,12 +27,20 @@ public class PlayerInfo {
 		return velocity;
 	}
 
+	public boolean isPathValid() {
+		return pathValid;
+	}
+
 	public void setPosition(Point newPos) {
 		this.newPos = newPos;
 	}
 
 	public void setVelocity(Vec2D newVelocity) {
 		this.velocity = newVelocity;
+	}
+
+	public void setPathValid(boolean valid) {
+		pathValid = valid;
 	}
 
 }
