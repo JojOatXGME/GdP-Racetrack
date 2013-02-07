b8fd0bb903b5ec69ab41abac103418d62dfaa4d3
Nikita Rose <roseniki@spandau.informatik.hu-berlin.de>
2013-02-07 12:11:21 +0100

    add PlayerCollision_PhaseThrough.java

---
+++ src/gdp/racetrack/PlayerCollision_PhaseThrough.java
@@ -0,0 +1,11 @@
+package gdp.racetrack;
+
+public class PlayerCollision_PhaseThrough implements PlayerCollisionRule {
+	
+	@Override
+	public void init(Game game) {}
+
+	@Override
+	public void handleCollision(PlayerInfo actor, PlayerInfo victim) {}
+
+}
